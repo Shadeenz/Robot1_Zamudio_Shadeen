86a3bbb25342382877c50f643c2908c185836966
Shadeenz <al1701002@upy.edu.mx>
2019-06-30 11:24:01 -0500

    Finished robot turn 90° degrees

--- controllers/robot1/robot1.c
+++ controllers/robot1/robot1.c
@@ -14,102 +14,126 @@
 #include <webots/motor.h>
 #include <webots/keyboard.h>
 #include <webots/distance_sensor.h>
+#include <webots/position_sensor.h>
 
 #include <stdio.h>
+#include <math.h>
 /*
  * You may want to add macros here.
  */
 #define TIME_STEP 64
+#define PI 3.1416
 
-/*
- * This is the main program.
- * The arguments of the main function can be specified by the
- * "controllerArgs" field of the Robot node
- */
+#define OBSTACLE_DIST 20.0
+
+
+/*variables*/
+enum {
+  GO,
+  TURN,
+  FREEWAY,
+  OBSTACLE
+};
+double straightLineAngle;
+
+
+/*Auxiliar functions*/
+int searchForObstacles(WbDeviceTag distance_sensor) {
+  double distance_of_sensor = wb_distance_sensor_get_value(distance_sensor);
+
+  if (distance_of_sensor > OBSTACLE_DIST)
+    return FREEWAY;
+  else
+    return OBSTACLE;
+}
+
+void advanceStraightLineRobot(WbDeviceTag *wheels, double velocity) {
+  wb_motor_set_velocity(wheels[0], -velocity);
+  wb_motor_set_velocity(wheels[1], -velocity);
+}
+
+void stopWheels(WbDeviceTag *wheels) {
+  wb_motor_set_velocity(wheels[0], 0);
+  wb_motor_set_velocity(wheels[1], 0);
+}
+
+void wheelsTurnRight(WbDeviceTag *wheels) {
+  //double initial_position = wb_position_sensor_get_value()
+  wb_motor_set_velocity(wheels[0], -6.66);
+  wb_motor_set_velocity(wheels[1], 6.66);
+}
+
+double getAngleRobot(WbDeviceTag pos_sensor) {
+  printf("Angle calculation\n");
+  double angle, rotationAngleW1;
+
+  rotationAngleW1 = wb_position_sensor_get_value(pos_sensor);
+  angle = fabs(rotationAngleW1- straightLineAngle);
+  printf("Angle: %lf\n", angle);
+
+  return angle;
+}
+
+float clearAngleRobot() {
+  printf("Clearing angle\n");
+}
+
 int main(int argc, char **argv)
 {
   /* necessary to initialize webots stuff */
   wb_robot_init();
 
-  //keyboard devices
-  wb_keyboard_enable(TIME_STEP);
-  int pressed_key;
-
-  /*
-   * You should declare here WbDeviceTag variables for storing
-   * robot devices like this:
-   *  WbDeviceTag my_sensor = wb_robot_get_device("my_sensor");
-   *  WbDeviceTag my_actuator = wb_robot_get_device("my_actuator");
-   */
    //Motor devices
    WbDeviceTag wheel_right= wb_robot_get_device("motor_right");
    WbDeviceTag wheel_left= wb_robot_get_device("motor_left");
 
+   WbDeviceTag wheels[2];
+   wheels[0] = wheel_right;
+   wheels[1] = wheel_left;
+
    wb_motor_set_position(wheel_right, INFINITY);
    wb_motor_set_position(wheel_left, INFINITY);
 
+   float velocity = 6.66;
+   int robot_state = GO;
+
    //distance sensor devices
    WbDeviceTag dist_sensor=wb_robot_get_device("distance_sensor");
-
    wb_distance_sensor_enable(dist_sensor, TIME_STEP);
+   double distance_value;
 
-   double ds_value;
-  /* main loop
-   * Perform simulation steps of TIME_STEP milliseconds
-   * and leave the loop when the simulation is over
-   */
+  //encoder device
+   WbDeviceTag encoder = wb_robot_get_device("encoder1");
+   wb_position_sensor_enable(encoder, TIME_STEP);
+
+   float angle;
+
+  /* main loop*/
   while (wb_robot_step(TIME_STEP) != -1) {
 
-    /*
-     * Read the sensors :
-     * Enter here functions to read sensor data, like:
-     *  double val = wb_distance_sensor_get_value(my_sensor);
-     */
-     
-    /*Keyboard sensor*/
-    pressed_key = wb_keyboard_get_key();
-   
-   /*Read distance sensor*/ 
-    ds_value = wb_distance_sensor_get_value(dist_sensor);
-    printf("Distance sensor: %lf\n", ds_value);
-    /* Process sensor data here */
+    if (robot_state == GO) {
+      distance_value = searchForObstacles(dist_sensor);
 
-    /*
-     * Enter here functions to send actuator commands, like:
-     * wb_differential_wheels_set_speed(100.0,100.0);
-     */
-     
-    /*Keyboard conditions*/
+      if (distance_value== FREEWAY) {
+        advanceStraightLineRobot(wheels, velocity);
+        angle = wb_position_sensor_get_value(encoder);
+        printf("Angle: %lf\n", angle);
+      } else if (distance_value== OBSTACLE) {
+        robot_state = TURN;
+        stopWheels(wheels);
+        straightLineAngle = wb_position_sensor_get_value(encoder);
+      }
+    } else if (robot_state == TURN) {
+      wheelsTurnRight(wheels);
+      angle = getAngleRobot(encoder);
 
-    if (pressed_key == WB_KEYBOARD_UP){
-     wb_motor_set_velocity(wheel_right, -6.66);
-     wb_motor_set_velocity(wheel_left,  -6.66);
-     }
-     
-     if (ds_value <= 20.000000){
-       wb_motor_set_velocity(wheel_left, -6.66);
-       wb_motor_set_velocity(wheel_right, 6.66);
-     }
-     
-     
-    if (pressed_key==WB_KEYBOARD_DOWN){
-     wb_motor_set_velocity(wheel_right, 6.66);
-     wb_motor_set_velocity(wheel_left,  6.66);
-     }
-     
-    if (pressed_key==WB_KEYBOARD_RIGHT){
-     wb_motor_set_velocity(wheel_left, -6.66);
-     wb_motor_set_velocity(wheel_right, 6.66);
-     }
-     
-    if (pressed_key==WB_KEYBOARD_LEFT){
-     wb_motor_set_velocity(wheel_left,   6.66);
-     wb_motor_set_velocity(wheel_right, -6.66);
-     }
-
-
-
-  };
+      if (angle >= 0.8*PI) {
+        robot_state = GO;
+        stopWheels(wheels);
+        clearAngleRobot();
+      }
+    }
+  }
 
   /* Enter your cleanup code here */
 
@@ -118,3 +142,4 @@ int main(int argc, char **argv)
 
   return 0;
 }
+  
